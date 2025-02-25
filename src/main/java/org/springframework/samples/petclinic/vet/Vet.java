f27d06dc98a5bf034ccdc852a3716127b1a20d27
jample <smartljb@hotmail.com>
2025-02-25 13:34:02 +0800

    Remove MySQL and PostgreSQL database setup files and update application properties for Thymeleaf configuration

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ // just for demo
 package org.springframework.samples.petclinic.vet;
 
 import java.util.Comparator;
